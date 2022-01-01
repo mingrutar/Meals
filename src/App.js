97976be6e4871a4fe14a98224d314a19d34536a0
Ming Rutar <rutar.ming@gmail.com>
2021-12-31 19:49:32 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <div>Image</div>
+      </header>
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
